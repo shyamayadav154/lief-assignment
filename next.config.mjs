d0cc771c115408d2a7bb5563de187405aa4830d7
shyam <shyamayadav154@gmail.com>
2023-06-17 12:18:23 +0530

    feat: pwa config added, type and lint check disabled

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,38 @@
  * for Docker builds.
  */
 await import("./src/env.mjs");
+import withPWA from "next-pwa";
+
+/** @type {import("next-pwa").PWAConfig} */
+const pwaConfig = {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
+};
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        domains: ["lh3.googleusercontent.com"],
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+    /**
+     * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+     * out.
+     *
+     * @see https://github.com/vercel/next.js/issues/41980
+     */
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
 };
-export default config;
+export default withPWA(pwaConfig)(config);
